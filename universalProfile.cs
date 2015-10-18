e873db607b16c4a2dbdbfe2483a22a91e222bc69
wbulot <wbulot@hotmail.com>
2015-10-18 18:23:43 +0200

    Update
    Minor update for Cruel taskmaster

--- universalProfile.cs
+++ universalProfile.cs
@@ -652,13 +652,10 @@ namespace SmartBot.Plugins.API
                     break;
 
 		        case Card.Cards.EX1_603://Cruel Taskmaster
-                    if (ArchetypeManager.GetFriendlyArchetype(board) == ArchetypeManager.Archetype.WorgenOtk)
+                    GlobalValueModifier -= 8;
+                    if (board.Hand.FindAll(x => x.Template.Id == Card.Cards.EX1_603).Count == 2) //If we have 2 taskmaster, increase cast value
                     {
-                        GlobalValueModifier -= 8;
-                        if (board.Hand.FindAll(x => x.Template.Id == Card.Cards.EX1_603).Count == 2) //If we have 2 taskmaster, increase cast value
-                        {
-                            GlobalValueModifier += 4;
-                        }
+                        GlobalValueModifier += 4;
                     }
                     break;
 
